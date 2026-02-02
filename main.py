02a90d7bef49fb25e0d2df108d5a267b2a860ab3
Hugo Brygo <hubrygo@fu-r2-p7.s19.be>
2026-02-02 11:25:35 +0100

    add main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+
+allowed_moves = {"R","R'","R2","L","L'","L2","U","U'","U2",
+           "D","D'","D2","F","F'","F2","B","B'","B2"}
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py \"R F B2 F'\"")
+        sys.exit(1)
+    shuffle = sys.argv[1]
+    invalid_moves = [move for move in shuffle.split() if move not in allowed_moves]
+    if invalid_moves:
+        print(f"Invalid moves found: {', '.join(invalid_moves)}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
