5d65585549056ed41c4d8b5e9452e2267b9274c6
Hugo Brygo <hubrygo@fu-r2-p7.s19.be>
2026-02-03 13:31:35 +0100

    new test file

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
-for i in $(seq 1 100);
+for i in $(seq 1 500);
 do
-    python3 main.py "$(python3 bonus_shuffle.py 21)" --fast
+    python3 main.py "$(python3 bonus_shuffle.py 31)" --fast
 done
+
+
+# F L2 D2 F2 L B L2 F B' F U2 L2 B' L U' D' B' U L R' U D2 L' R2 L B2 R' F' R2 D2
