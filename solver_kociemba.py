fe525c312a68a9882e50aa05e3a30cbd705fdcae
Hugo Brygo <hubrygo@fu-r2-p7.s19.be>
2026-02-03 13:54:14 +0100

    patch: EXCEPTION: can only concatenate str (not NoneType) to str

--- solver_kociemba.py
+++ solver_kociemba.py
@@ -1057,7 +1057,10 @@ class Search:
         """Convertit la solution en notation standard"""
         parts = []
         for i in range(length):
-            parts.append(self.AXIS_NAMES[self.ax[i]] + self.POWER_NAMES[self.po[i]])
+            if self.POWER_NAMES[self.po[i]] is not None:
+                parts.append(self.AXIS_NAMES[self.ax[i]] + self.POWER_NAMES[self.po[i]])
+            else:
+                parts.append(self.AXIS_NAMES[self.ax[i]])
             if i == sep_pos - 1:
                 parts.append('. ')
         return ''.join(parts).strip()
