2f68e1d0b14a10851c30e363f8e999cc83b9f7c6
Hugo Brygo <hubrygo@fu-r2-p7.s19.be>
2026-02-04 10:17:02 +0100

    Revert "new test file"
    This reverts commit 5d65585549056ed41c4d8b5e9452e2267b9274c6.

--- test.sh
+++ test.sh
@@ -1,7 +1,4 @@
-for i in $(seq 1 500);
+for i in $(seq 1 100);
 do
-    python3 main.py "$(python3 bonus_shuffle.py 31)" --fast
+    python3 main.py "$(python3 bonus_shuffle.py 21)" --fast
 done
-
-
-# F L2 D2 F2 L B L2 F B' F U2 L2 B' L U' D' B' U L R' U D2 L' R2 L B2 R' F' R2 D2
