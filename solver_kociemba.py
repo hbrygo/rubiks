1025352920f4f82095ba9f0e23521215c7eb3c53
JNTHNN <j.gasparoly@gmail.com>
2026-02-16 10:04:02 +0100

    fix: optimal kociemba

--- solver_kociemba.py
+++ solver_kociemba.py
@@ -727,7 +727,15 @@ class Search:
         self.minDistPhase1[1] = 1
         n = 0
         busy = False
-        depth_phase1 = 1
+
+        # Start at the pruning lower bound to skip impossible depths.
+        init_estimate = max(
+            get_pruning(self.tables.slice_flip_prun,
+                        N_SLICE1 * self.flip[0] + self.slice_[0]),
+            get_pruning(self.tables.slice_twist_prun,
+                        N_SLICE1 * self.twist[0] + self.slice_[0])
+        )
+        depth_phase1 = max(1, init_estimate)
         
         t_start = time.time()
         
